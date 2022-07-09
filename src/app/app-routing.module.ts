aaf398ba126fc3e875669042d7eaefb47d85f382
gmaciel <gmaciel@band.com.br>
2022-07-09 17:12:34 -0300

    subindo primeira versao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PicturesComponent } from './pages/pictures/pictures.component';
+import { VideosComponent } from './pages/videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'videos', component: VideosComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'pictures', component: PicturesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { PicturesComponent } from './pages/pictures/pictures.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VideosComponent,
+    PicturesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
